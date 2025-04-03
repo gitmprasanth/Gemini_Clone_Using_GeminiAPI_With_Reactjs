37ddc7a6f0613f50a86186959279ceae847922a0
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-04-03 17:58:35 +0530

    initial Set UP

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  base:"/Gemini_Clone_Using_GeminiAPI_With_Reactjs"
+})
